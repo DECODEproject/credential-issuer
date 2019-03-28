f257f51271d49ce00c3bf2d3bf14ee90da1facbc
Puria Nafisi Azizi <puria@dyne.org>
2019-03-28 13:36:06 +0100

    :rewind: Revert contracts to the zenroom script again in place of zencodes

--- app/zencontract.py
+++ app/zencontract.py
@@ -52,7 +52,7 @@ class ZenContract(object):
             log.debug("KEYS: %s" % self.keys())
             log.debug("CODE: \n%s" % self.zencode.decode())
         try:
-            result, errors = zenroom.zencode(
+            result, errors = zenroom.execute(
                 self.zencode, keys=self._keys, data=self._data
             )
             self._error = errors
